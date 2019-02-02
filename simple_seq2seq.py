3b68d88f4cfe6f1cab416bd8b7b874855200bced
Raviraja <raviraja.ghanta@gmail.com>
2019-02-02 10:18:22 +0530

    decoder implemented

--- simple_seq2seq.py
+++ simple_seq2seq.py
@@ -81,6 +81,7 @@ class Encoder(nn.Module):
         dropout: A float indicating dropout.
     '''
     def __init__(self, input_dim, emb_dim, hidden_dim, n_layers, dropout):
+        super().__init__()
         self.input_dim = input_dim
         self.emb_dim = emb_dim
         self.hidden_dim = hidden_dim
@@ -102,8 +103,61 @@ class Encoder(nn.Module):
         # input is of shape [sequence_length, batch_size, input_size]
         # hidden is of shape [num_layers * num_directions, batch_size, hidden_size]
         # cell is of shape [num_layers * num_directions, batch_size, hidden_size]
-        outputs, (hidden, cell) = self.rnn(embedded) 
+        outputs, (hidden, cell) = self.rnn(embedded)
 
         # outputs are always from the top hidden layer, if bidirectional outputs are concatenated.
         # outputs shape [sequence_length, batch_size, hidden_dim * num_directions]
         return hidden, cell
+
+
+class Decoder(nn.Module):
+    ''' This class contains the implementation of Decoder Module.
+
+    Args:
+        output_dim: A integer indicating the size of output dimension.
+        embedding_dim: A integer indicating the embedding size.
+        hidden_dim: A integer indicating the hidden size of rnn.
+        n_layers: A integer indicating the number of layers in rnn.
+        dropout: A float indicating the dropout.
+    '''
+    def __init__(self, embedding_dim, output_dim, hidden_dim, n_layers, dropout):
+        super().__init__()
+
+        self.embedding_dim = embedding_dim
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        self.embedding = nn.Embedding(output_dim, embedding_dim)
+        self.rnn = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=dropout)
+        self.linear = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, input, hidden, cell):
+        # input is of shape [batch_size]
+        # hidden is of shape [n_layer * num_directions, batch_size, hidden_size]
+        # cell is of shape [n_layer * num_directions, batch_size, hidden_size]
+
+        input = input.unsqueeze(0)
+        # input shape is [1, batch_size]. reshape is needed rnn expects a rank 3 tensors as input.
+        # so reshaping to [1, batch_size] means a batch of batch_size each containing 1 index.
+
+        embedded = self.embedding(input)
+        embedded = self.dropout(embedded)
+        # embedded is of shape [1, batch_size, embedding_dim]
+
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
+        # generally output shape is [sequence_len, batch_size, hidden_dim * num_directions]
+        # generally hidden shape is [num_layers * num_directions, batch_size, hidden_dim]
+        # generally cell shape is [num_layers * num_directions, batch_size, hidden_dim]
+
+        # sequence_len and num_directions will always be 1 in the decoder.
+        # output shape is [1, batch_size, hidden_dim]
+        # hidden shape is [num_layers, batch_size, hidden_dim]
+        # cell shape is [num_layers, batch_size, hidden_dim]
+
+        predicted = self.linear(output.squeeze(0))  # linear expects as rank 2 tensor as input
+        # predicted shape is [batch_size, output_dim]
+
+        return predicted, hidden, cell
